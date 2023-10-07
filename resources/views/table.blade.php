f281b01333b3ce212573441d819d137e62d1d4f9
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-10-07 19:08:28 +0100

    more

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -22,101 +22,36 @@
     <link href="../build/css/custom.min.css" rel="stylesheet">
   </head>
 
-  <body class="nav-md">
-    <div class="container body">
+  <body class="" style="bg-white">
+    <div class=""style>
       <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
-            </div>
-
-            <div class="clearfix"></div>
-
-        
-
-            <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-            <!-- /menu footer buttons -->
-          </div>
-        </div>
-
+      
         <!-- page content -->
         <div class="right_col" role="main">
           <div class="">
             <div class="page-title">
-              <div class="title_left">
-                <h3>Tables <small>Some examples to get you started</small></h3>
-              </div>
-
-              <div class="title_right">
-                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                  </div>
-                </div>
-              </div>
+            
+              
             </div>
 
-            <div class="clearfix"></div>
-
-           
-
-
-          
-              <div class="clearfix"></div>
 
               <div class="col-md-12 col-sm-12  ">
                 <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Table design <small>Custom design</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Settings 1</a>
-                            <a class="dropdown-item" href="#">Settings 2</a>
-                          </div>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-
+               
                   <div class="x_content">
 
-                    <p>Add class <code>bulk_action</code> to table for bulk actions options on row select</p>
-
+              
                     <div class="table-responsive">
                       <table class="table table-striped jambo_table bulk_action">
                         <thead>
                           <tr class="headings">
                             <th>
-                              <input type="checkbox" id="check-all" class="flat">
+                              <td class="column-title">sn</td>
                             </th>
-                            <th class="column-title">Invoice </th>
-                            <th class="column-title">Invoice Date </th>
-                            <th class="column-title">Order </th>
-                            <th class="column-title">Bill to Name </th>
+                            <th class="column-title">Name </th>
+                            <th class="column-title">Email </th>
+                            <th class="column-title">Phone </th>
+                            <th class="column-title">Title </th>
                             <th class="column-title">Status </th>
                             <th class="column-title">Amount </th>
                             <th class="column-title no-link last"><span class="nobr">Action</span>
@@ -131,15 +66,17 @@
                           @foreach($builds as $build)
                           <tr class="even pointer">
                             <td class="a-center ">
-                              <input type="checkbox" class="flat" name="table_records">
+                               <td class=" ">{{$build->id}}</td>
                             </td>
                             <td class=" ">{{$build->name}}</td>
-                            <td class=" ">May 23, 2014 11:47:56 PM </td>
-                            <td class=" ">121000210 <i class="success fa fa-long-arrow-up"></i></td>
-                            <td class=" ">John Blank L</td>
+                            <td class=" ">{{$build->email}} </td>
+                            <td class=" ">{{$build->phone}} <i class="success fa fa-long-arrow-up"></i></td>
+                            <td class=" "> {{$build->title}} </td>
                             <td class=" ">Paid</td>
                             <td class="a-right a-right ">$7.45</td>
-                            <td class=" last"><a href="#">View</a>
+                            <td class=" ">
+                              <a class ="btn btn-danger" type="submit" href="#">Delete</a>
+                              <a class ="btn btn-primary" type="submit" href="#">Accept</a>
                             </td>
                           </tr>
                       @endforeach
@@ -157,12 +94,7 @@
         <!-- /page content -->
 
         <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
+   
         <!-- /footer content -->
       </div>
     </div>
